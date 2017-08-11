80b39db8fd2d06c3690b34542550f6e1889341d1
MatOmega <30880484+MatOmega@users.noreply.github.com>
2017-08-11 15:36:52 +0200

    Update bot.js

--- .gitignore/bot.js
+++ .gitignore/bot.js
@@ -1,15 +1,32 @@
 const Discord = require('discord.js');
 const settings = require('./settings.json');
-const prefix = 'B!'
-const bot = new Discord.Client();
+const prefix = "!"
+const bot = new Discord.Client({disableEveryone: true});
 
 bot.on('ready', function() {
-    console.log('I am ready BOSS');
+    console.log("I'm Online");
 });
 
-bot.on('message', message => {
-	let args = message.content.split(' ').slice(1);
-	var result = args.join(' ');
+bot.on("message", async message => {
+	if(message.author.bot) return;
+	if(message.channel.type === "dm") return;
+	
+	let messageArray = message.content.split(" ");
+	let command = messageArray[0];
+	let args = messageArray.slice(1);
+
+	if(!command.startsWith(prefix)) return;
+	
+	if(command === `${prefix}userinfo`) {
+		let embed = new Discord.RichEmbed()
+			.setAuthor(message.author.username)
+			.setColor("#8B59B6")
+			.addField("Full Username", `${message.author.username}#${message.author.discriminator}`)
+			.addField("ID", message.author.id)
+			.addField("Created At", message.author.createdAt);
+			
+		message.channel.sendEmbed(embed);
+	} else
 
 	if (!message.content.startsWith(prefix)) return;
 	if (message.author.bot) return;
@@ -23,15 +40,8 @@ bot.on('message', message => {
 			result = null;
 		}
 		bot.user.setGame(result);
-	} else
-
-	if (message.content.startsWith(prefix + 'AbCdsetstatus')) {
-		if (!result) {
-			result = 'online';
-		}
-		bot.user.setStatus(result);
 	}
-
 });
 
 bot.login(settings.token);
+
