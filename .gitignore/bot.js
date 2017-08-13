7d5f19b94c908e930c60e48dbcc86c14444620fb
MatOmega <30880484+MatOmega@users.noreply.github.com>
2017-08-13 19:56:39 +0200

    Update bot.js

--- .gitignore/bot.js
+++ .gitignore/bot.js
@@ -2,6 +2,7 @@ const Discord = require('discord.js');
 const settings = require('./settings.json');
 const prefix = "!"
 const bot = new Discord.Client({disableEveryone: true});
+const { RichEmbed } = require('discord.js'),
 
 bot.on('ready', function() {
     console.log("I'm Online");
@@ -15,33 +16,81 @@ bot.on("message", async message => {
 	let command = messageArray[0];
 	let args = messageArray.slice(1);
 
-	if(!command.startsWith(prefix)) return;
-	
-	if(command === `${prefix}userinfo`) {
-		let embed = new Discord.RichEmbed()
-			.setAuthor(message.author.username)
-			.setColor("#8B59B6")
-			.addField("Full Username", `${message.author.username}#${message.author.discriminator}`)
-			.addField("ID", message.author.id)
-			.addField("Created At", message.author.createdAt);
-			
-		message.channel.sendEmbed(embed);
-	} else
+	if(!command.startsWith(settings.prefix)) return;
 
-	if (!message.content.startsWith(prefix)) return;
+	if (!message.content.startsWith(settings.prefix)) return;
 	if (message.author.bot) return;
 
-	if (message.content.startsWith(prefix + 'ping')) {
+	if (message.content.startsWith(settings.prefix + 'ping')) {
 		message.channel.sendMessage(`Pong! \`${Date.now() - message.createdTimestamp} ms\``);
-	} else
+	};
 
-	if (message.content.startsWith(prefix + 'AbCdsetgame')) {
-		if (!result) {
-			result = null;
-		}
-		bot.user.setGame(result);
+bot.on('message', message => {
+	if(message.content.startsWith(settings.prefix + "info")) {
+	message.channel.sendEmbed({
+			author: {
+			name: bot.user.username,
+			icon_url: bot.user.avatarURL
+		},
+			filds: [{
+			name: 'Guilds',
+			value: `I am In ${bot.guilds.size} Guilds`,
+			inline: true
+		},
+		{
+			name: 'Total users',
+			value: `${bot.guilds.reduce((p, c) => c.memberCount, 0)} users`,
+			inline: true
+		},
+		{
+			name: 'MatΩ𝖒𝖊𝖌𝖆',
+			value: `<@127439037383311360`,
+			inline: true
+		},
+		{
+			name: 'Uptime',
+			value: `${require('ms')(bot.uptime, {long:true})}`,
+			inline: true
+	}]
+})}});
+
+	bot.on('message', message => {
+		if(message.content.startsWith(settigns.prefix + 'serverinfo')) {
+			message.channel.send({
+				embed: {
+				color: 11206655,
+				author: {
+					name: `${message.guild.name}`,
+					icon_url: message.guild.iconURL
+					},
+					"thumbnail": {
+					"url": `${message.guild.iconURL}`
+					},
+					filds: [{
+					name: "Default Guild Channel",
+					value: `${message.guild.defaultChannel}`
+					},
+					{
+					name: "Guild Owner",
+					value: `${message.guild.owner}`
+					},
+					{
+					name: "Total Amount of Members",
+					value: `${message.guild.memberCount} Members`
+					},
+					{
+					name: "Server Made",
+					value: `${message.guild.createdAt.toLocaleString()}`
+					},
+				],
+				timestamp: new Date(),
+				footer: {
+					icon_url: bot.user.avatarURL,
+					text: `${message.guild.name} is a group by ${bot.user.username}`
+				}
+			}
+		})
 	}
 });
 
 bot.login(settings.token);
-
